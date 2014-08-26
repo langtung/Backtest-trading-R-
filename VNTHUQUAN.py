64cf9804d9b491ff6bdfbcd534620f51c9c2927c
langtung <nguyendangphu@hotmail.co.uk>
2014-08-26 18:16:05 +0700

    Update VNTHUQUAN.py

--- VNTHUQUAN.py
+++ VNTHUQUAN.py
@@ -27,6 +27,27 @@ while stop1<100:
 
 	
 
+from selenium import webdriver
+from decimal import *
+driver = webdriver.Firefox()
+driver.get("http://vnthuquan.net/truyen/")
+text=[]
+stop=0
+stop1=0
+
+	
+while stop1<20:
+	text.append(driver.page_source)
+	stop1=Decimal((driver.find_element_by_xpath("//td[@colspan='4']/span").text))
+	if ((stop1%10)==0):
+		u=unicode(Decimal(stop1+1))
+		print u
+		b=driver.find_element_by_xpath("//td[@colspan='4']/a[contains(.,'"+u+"')]").click()
+	else:
+		u=u'...'
+		b=driver.find_elements_by_xpath("//td[@colspan='4']/a[contains(.,u)]")[1].click()
+	print b
+		
 
 
 
