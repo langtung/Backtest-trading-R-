1724372a4825d7d227a79b95a113ee2aa05f9a5c
langtung <nguyendangphu@hotmail.co.uk>
2014-11-19 14:20:15 +0700

    Update test.R

--- test.R
+++ test.R
@@ -29,6 +29,33 @@ library(reshape2)
   )
 
 #--------------------------------------------------------------------
+bt=function(ticker){
+X=ticker
+a=trade(date=as.Date('2000-01-01'),trade_stt="NA")
+nrow1=nrow(X)
+names(X)[6]='Adj.Close'
+X_C=X[,6]
+date1=as.Date(index(X),f='%Y-%m-%d')
+X_C=as.data.frame(X_C)
+X_C=data.table(X_C)
+X_C[,SMA20:=SMA(Adj.Close,50)]
+X_C[,check:=ifelse(Adj.Close>SMA20,"Buy","Sell")]
+X_C
+X_C$date=date1
+X_C
+X_C$date=as.Date(X_C$date)
+lapply(51:nrow1,function(x){a$check_(new_date=X_C$date[x],check=X_C$check[x])})
+result=a$result
+result =merge(x=result,y=X_C,by='date')
+result$lag1=lag(result$Adj.Close,1)
+result$return=result$Adj.Close/result$lag1-1
+result.final=subset(result,check=="Sell")
+b=result.final$return; b[is.na(b)]=0
+cum1=cumprod(1+b)
+print(result)
+print(cum1)
+return(result.final)}
+#----------------------------------------------------------------------
 
 ED_C=ED[,6]
 ED_C=as.data.frame(ED_C)
